2f27bd9b80d82799e4e60bf63dc83d492609f905
abobdadjimo <abobdadjimo@iqera.com>
2024-07-26 17:47:33 +0400

    Add TSID converter

--- src/main/java/com/iqera/cop/config/TsidConfiguration.java
+++ src/main/java/com/iqera/cop/config/TsidConfiguration.java
@@ -1,10 +1,15 @@
 package com.iqera.cop.config;
 
+import com.github.f4b6a3.tsid.Tsid;
 import com.github.f4b6a3.tsid.TsidFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class TsidConfiguration {
+public class TsidConfiguration implements WebMvcConfigurer {
 
     //@Value("${application.tsid.discord-snowflakes-worker}")
     private int discordSnowflakesWorkerId = 1;
@@ -28,5 +33,18 @@ public class TsidConfiguration {
 
     public static void setDiscordSnowflakesFactory(TsidFactory discordSnowflakesFactory) {
         TsidConfiguration.discordSnowflakesFactory = discordSnowflakesFactory;
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        Converter<String, Tsid> converter = s -> {
+            Tsid tsid = null;
+            if (StringUtils.hasText(s)) {
+                tsid = Tsid.from(s);
+            }
+            return tsid;
+        };
+
+        registry.addConverter(String.class, Tsid.class, converter);
     }
 }
